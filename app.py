3173d355696c8ba758047258702a4442ecb425b4
Alimustoofaa <softhouse.ali@gmail.com>
2023-06-05 01:49:19 +0700

    first commit

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ pathlib.Path(SAVE_FOLDER).mkdir(parents=True, exist_ok=True)
 def display_pdf(file):
 	with open(file, "rb") as f:
 		base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-	pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="900" type="application/pdf">'
+	pdf_display =  F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
 	st.markdown(pdf_display, unsafe_allow_html=True)
 
 # Input file upload
